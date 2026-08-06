1565d0f770fe688c252290794a4067fb4dde792b
sanedoma <ous.ndiaye470@gmail.com>
2026-08-06 14:43:16 +0200

    feat: ajout du CRUD et du error Handling global

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express();
 
 
 const tasksRoutes = require("./routes/tasks");
+const errorHandler = require("./middleware/errorHandler");
 
 
 app.use(express.json());
@@ -16,5 +17,5 @@ app.get("/", (req, res) => {
     res.send("Bienvenue sur la Todo API !");
 });
 
-
+app.use(errorHandler);
 module.exports = app;
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -11,5 +11,70 @@ router.get("/", (req, res) => {
 
 });
 
+router.post("/", (req, res) => {
+
+    const newTask = {
+        id: tasks.length + 1,
+        title: req.body.title,
+        completed: false
+    };
+
+
+    tasks.push(newTask);
+
+
+    res.status(201).json(newTask);
+
+});
+
+router.delete("/:id", (req, res) => {
+
+    const taskId = Number(req.params.id);
+
+
+    const taskIndex = tasks.findIndex(
+        task => task.id === taskId
+    );
+
+
+    if (taskIndex === -1) {
+        return res.status(404).json({
+            message: "Task not found"
+        });
+    }
+
+
+    const deletedTask = tasks.splice(taskIndex, 1);
+
+
+    res.json(deletedTask[0]);
+
+});
+
+router.put("/:id", (req, res) => {
+
+    const taskId = Number(req.params.id);
+
+
+    const task = tasks.find(
+        task => task.id === taskId
+    );
+
+
+    if (!task) {
+        return res.status(404).json({
+            message: "Task not found"
+        });
+    }
+
+
+    task.title = req.body.title ?? task.title;
+
+    task.completed = req.body.completed ?? task.completed;
+
+
+    res.json(task);
+
+});
 
 module.exports = router;
