8fe6166c7f271980e013e0bd85dfea2009968994
sanedoma <ous.ndiaye470@gmail.com>
2026-08-06 14:29:15 +0200

    refactor:  les taches dans le task model

---
+++ src/models/task.js
@@ -0,0 +1,15 @@
+const tasks = [
+    {
+        id: 1,
+        title: "Apprendre Node.js",
+        completed: false
+    },
+    {
+        id: 2,
+        title: "Créer une API",
+        completed: true
+    }
+];
+
+
+module.exports = tasks;
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,21 +1,15 @@
 const express = require("express");
 
 const router = express.Router();
+
+const tasks = require("../models/task");
+
+
 router.get("/", (req, res) => {
 
-    res.json([
-        {
-            id: 1,
-            title: "Apprendre Node.js",
-            completed: false
-        },
-        {
-            id: 2,
-            title: "Créer une API",
-            completed: true
-        }
-    ]);
+    res.json(tasks);
 
 });
+
 
 module.exports = router;
